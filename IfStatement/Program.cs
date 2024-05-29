a0e557515230a23b2f6483a977cc5e3b58206e0d
Fahim <fahimshahariar.fs@gmail.com>
2024-05-29 18:48:07 +0600

    day3

---
+++ IfStatement/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace IfStatement
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Enter an amount you want to invest: ");
+            int amount = Convert.ToInt32(Console.ReadLine());
+
+            Console.Write("Enter the number of years: ");
+            int year = Convert.ToInt32(Console.ReadLine());
+            int month = year * 12;
+
+            int interest = 9;
+
+            Double income = (amount * month) * interest / 100;
+
+            Double finalReturn = income + (amount * month);
+
+            if (finalReturn <= 150000)
+            {
+                Console.WriteLine(finalReturn);
+                Console.WriteLine("You will not get a good return!");
+            }
+            else
+            {
+                Console.WriteLine(finalReturn);
+                Console.WriteLine("You will get a good return!");
+            }
+
+            Console.ReadLine();
+
+        }
+    }
+}
