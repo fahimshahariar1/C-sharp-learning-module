75775282f0af9082ed79f2390379229739054dc3
Fahim <fahimshahariar.fs@gmail.com>
2024-07-27 11:26:15 +0600

    day 6

---
+++ FizzbuzzGame/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FizzbuzzGame
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            
+
+            for (int i = 1; i <= 15; i++)
+            {
+                bool threediv = i % 3 == 0;   // for better readibility and if the condition changes top divide by other number we
+                bool fivediv = i % 5 == 0;   //can simply chnage it here. we won't have to change in everywhere
+
+                if (threediv && fivediv)
+                {
+                    Console.WriteLine("FizzBuzz");
+                }
+                else if (threediv)
+                {
+                    Console.WriteLine("Fizz");
+
+                }
+                else if (fivediv)
+                {
+                    Console.WriteLine("Buzz");
+
+                }
+                else { Console.WriteLine(i); }
+
+            }
+            Console.ReadLine();
+
+        }
+    }
+}
