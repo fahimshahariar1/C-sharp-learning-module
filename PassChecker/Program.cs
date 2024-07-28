98dddc2ac06112c57e23ef368981f97c1fa2f016
Fahim <fahimshahariar.fs@gmail.com>
2024-07-28 10:32:36 +0600

    day 7

---
+++ PassChecker/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PassChecker
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Console.Write("Enter your Password: ");
+            String pass = Console.ReadLine();
+
+            Console.Write("Enter Your Password Again: ");
+            String passcheck = Console.ReadLine();
+
+            if (passcheck.Equals(pass))
+            {
+                Console.WriteLine("Your password is correct");
+            }
+            else
+            {
+                Console.WriteLine("Your password is incorrect");
+            }
+
+            Console.ReadLine();
+        }
+    }
+}
