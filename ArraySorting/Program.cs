731e7cf42197db5386eec51e7d790284448cf0e7
Fahim <fahimshahariar.fs@gmail.com>
2024-07-30 09:43:01 +0600

    Arraysort

---
+++ ArraySorting/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ArraySorting
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+            int[] numbers = new int[] { 5,8,1,9 };
+
+            Array.Sort(numbers);
+
+            foreach (int i in numbers) {
+
+                Console.WriteLine(i);
+            }
+
+             
+
+            Console.ReadLine();
+
+        }
+    }
+}
--- Arrays/Program.cs
+++ Arrays/Program.cs
@@ -10,6 +10,27 @@ namespace Arrays
     {
         static void Main(string[] args)
         {
+
+            int[] angles = new int[3];
+
+            for (int i = 0; i < angles.Length; i++)
+            {
+                Console.Write($"Enter angle {angles[i + 1]}: ");
+                angles[i] = Convert.ToInt32(Console.ReadLine());
+            }
+
+            int angleSum = 0;
+
+            foreach (int angle in angles)
+            {
+                angleSum += angle;
+            }
+
+            Console.WriteLine(angleSum);
+
+            Console.WriteLine(angleSum == 180 ? "Valid" : "Invalid");
+
+            Console.ReadLine();
         }
     }
 }
