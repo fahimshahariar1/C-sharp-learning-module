d98fad783a944329d03ab0cdcf4518d35e442e5f
Fahim <fahimshahariar.fs@gmail.com>
2024-07-31 09:16:19 +0600

    lists

---
+++ Lists/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lists
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+            List<int> numberslist = new List<int>()
+            { 
+                 1,2,3,4,5,6 
+            }; 
+
+            numberslist.Add(1);
+            Console.WriteLine(numberslist.Contains(2));
+
+
+            foreach (int number in numberslist)
+            {
+                Console.WriteLine(number);
+            }
+
+
+            Console.ReadLine();
+
+        }
+    }
+}
