7e6b2ff560fabf588ea60340924c6f6e8dbfa20a
Fahim <fahimshahariar.fs@gmail.com>
2024-08-01 10:29:53 +0600

    exercise

---
+++ OddEvenNumberSplit/Program.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OddEvenNumberSplit
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+            List<int> list1 = new List<int>() 
+            { 
+            
+            };
+
+            List<int> list2 = new List<int>()
+            {
+
+            };
+
+            for (int i = 0; i <= 20; i++)
+            {
+                if (i % 2 == 0) 
+                { 
+                list1.Add(i);
+                }
+                else
+                {
+                    list2.Add(i);
+                }
+
+               
+            }
+
+            Console.WriteLine("Printing Even Numbers:");
+
+            foreach (int i in list1)
+            {
+                Console.Write($"{i } ");
+            }
+
+
+            Console.WriteLine(Environment.NewLine+"Printing Odd Numbers:");
+
+            foreach (int i in list2)
+            {
+                Console.Write($"{i} ");
+            }
+
+
+            Console.ReadLine();
+
+
+        }
+    }
+}
