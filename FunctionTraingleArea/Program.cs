1343154ce08e32958c28a186bcc2c096e1066779
Fahim <fahimshahariar.fs@gmail.com>
2024-08-03 18:34:12 +0600

    Function Triangle Area

---
+++ FunctionTraingleArea/Program.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FunctionTraingleArea
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+            
+            double width = ReadInt("Enter Width: ");
+
+            
+            double height = ReadInt("Enter Height: ");
+
+            Console.Write($"The Area of triangle is: {Area(width, height)}");
+
+
+
+            Console.ReadLine();
+
+
+        }
+
+        static int ReadInt(String message) 
+        {
+
+            Console.Write(message);
+            return Convert.ToInt32(Console.ReadLine());
+        
+        }
+
+
+
+        static double Area(double width, double height)
+        {
+
+            return 0.5 * width * height;
+
+        }
+
+    }
+}
