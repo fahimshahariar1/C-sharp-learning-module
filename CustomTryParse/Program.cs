6fd19811eca153c6a950b24a144ac06e4e3e4c19
Fahim <fahimshahariar.fs@gmail.com>
2024-08-03 21:29:56 +0600

    CTP

---
+++ CustomTryParse/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CustomTryParse
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+            bool success = false;
+            
+            try
+            {
+                Console.Write("Enter Number: ");
+                int num = Convert.ToInt32(Console.ReadLine());
+                success = true;
+
+            }
+            catch (FormatException e)
+            {
+                Console.WriteLine(e.Message);
+               
+            }
+
+            Console.WriteLine(success ? "Yay" : "Oh no!");
+
+            Console.ReadLine();
+        }
+    }
+}
